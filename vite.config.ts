cce11a0ccf48687812208a7dfdefd8c09a7de4c3
Beast <idewagedebisma@gmail.com>
2025-12-03 18:53:51 +0800

    fix: formatting

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(),tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     port: 6060,
     host: true,
