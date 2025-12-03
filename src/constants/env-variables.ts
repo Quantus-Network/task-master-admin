cd49639e34eb0621c9f447095a4af4d70d5af643
Beast <idewagedebisma@gmail.com>
2025-12-03 19:08:38 +0800

    feat: remove unused env var

--- src/constants/env-variables.ts
+++ src/constants/env-variables.ts
@@ -1,5 +1,4 @@
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
-export const BASE_WEBSITE_URL = import.meta.env.VITE_WEBSITE_URL;
 
 export const API_URL = `${BASE_API_URL}/api`;
 export const X_BASE_URL = "https://x.com";
